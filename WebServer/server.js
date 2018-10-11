9af7f92a4e89f1033cc4e5f779317c5f78ecad24
mrkjdy <mrkjudy@gmail.com>
2018-10-11 11:02:42 -0400

    started working on server.js, added index.html, index.js, index.css, and scores.html

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,4 +1,17 @@
+const express = require('express');
 const mysql = require('mysql');
+const PORT = process.env.PORT || 5000;
+
+var app = express();
+var path = require('path');
+
+// Sets public as the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Creates the server
+app.listen(PORT, function() {
+	console.log("Listening on " + PORT)
+});
 
 // Database connection info
 var db = mysql.createPool({
