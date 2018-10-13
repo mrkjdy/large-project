7d9c9a277b88f0f4526d90d446416fe94e2ac4aa
Thomas Whitaker <25395161+Thomas-Whitaker@users.noreply.github.com>
2018-10-13 14:03:03 -0400

    Added session/favicon modules

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const PORT = process.env.PORT || 5000;
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const favicon = require('serve-favicon');
 
 var app = express();
 var path = require('path');
 
+// Body-parser initialization
+app.set('trust proxy', 1);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+	secret: 'T9LqJYlgFNQi46lBBGge',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		secure: true,
+		maxAge: 86400000
+	}
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Sets public as the public folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // Creates the server
 app.listen(PORT, function() {
