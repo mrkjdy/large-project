46a103cc63cf63e36872cec68cdd457077105d27
Hunter Tanchin <huntergt116@yahoo.com>
2018-10-31 21:13:34 -0400

    Fixed error in wear>...>Settings.java and mobile>...>styles.xml

--- StepswithFriends/wear/src/main/java/commrkjdylarge_project/github/stepwithfriends/Settings.java
+++ StepswithFriends/wear/src/main/java/commrkjdylarge_project/github/stepwithfriends/Settings.java
@@ -8,6 +8,6 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        //setContentView(R.layout.activity_settings);
     }
 }
