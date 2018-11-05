e0f15081f8a3951a7445684c5be664992016e346
Thomas Whitaker <25395161+Thomas-Whitaker@users.noreply.github.com>
2018-11-05 11:54:11 -0500

    Add sync logic to app

--- StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/Login.java
+++ StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/Login.java
@@ -35,6 +35,7 @@ public class Login extends AppCompatActivity {
                     params.put("username", username.getText().toString());
                     params.put("password", password.getText().toString());
                     client.post("https://large-project.herokuapp.com/login", params, new JsonHttpResponseHandler() {
+                        // TODO: Fix bug where login fails when heroku is idle
                         @Override
                         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                             // called when response HTTP status is "200 OK"
--- StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/SWFApp.java
+++ StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/SWFApp.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import com.loopj.android.http.*;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
@@ -13,6 +14,7 @@ public class SWFApp extends Application {
 
     private AsyncHttpClient asyncHttpClient = null;
     private JSONObject userData = null;
+    private boolean syncStatus = false;
 
     public AsyncHttpClient getClient() {
         if(this.asyncHttpClient == null) {
@@ -25,11 +27,13 @@ public class SWFApp extends Application {
         }
     }
 
+    // WARNING: Do not use this unless you are logging in, to update user data use updateUserData()
     public void setUserData(JSONObject data) {
         this.userData = data;
     }
 
     public JSONObject getUserData() {
+        // Gets user data if it somehow wasn't initialized
         if(this.userData == null) {
             this.asyncHttpClient.post("https://large-project.herokuapp.com/getuserdata", null, new JsonHttpResponseHandler() {
                 @Override
@@ -51,4 +55,67 @@ public class SWFApp extends Application {
         this.asyncHttpClient = null;
         this.userData = null;
     }
+
+    // Used to update one user value
+    public boolean updateUserData(String field, Object value) {
+        if(field == null || value == null) {
+            // need a field and value to update
+            return false;
+        } else {
+            if(syncUserData(new String[] {field}, new Object[] {value})) {
+                try {
+                    this.userData.put(field, value);
+                } catch(JSONException e) {
+                    return false;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Used to update multiple user values
+    public boolean updateUserData(String[] fields, Object[] values) {
+        if(fields == null || values == null || fields.length != values.length) {
+            // need a field and value of same size to update
+            return false;
+        } else {
+            if(syncUserData(fields, values)) {
+                try {
+                    for(int i=0; i<fields.length; i++) {
+                        this.userData.put(fields[i], values[i]);
+                    }
+                } catch(JSONException e) {
+                    return false;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private boolean syncUserData(Object[] fields, Object[] values) {
+        this.syncStatus = false;
+        RequestParams params = new RequestParams();
+        params.put("fields", fields);
+        params.put("values", values);
+        this.asyncHttpClient.post("https://large-project.herokuapp.com/updateuserdata", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                syncStatus = true;
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                syncStatus = false;
+            }
+        });
+        return this.syncStatus;
+    }
+
+
 }
