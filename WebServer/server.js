1a412d24d19b8897b01dca00ed1d3ba8b9dca1ae
Thomas Whitaker <25395161+Thomas-Whitaker@users.noreply.github.com>
2018-11-05 18:25:34 -0500

    Try string interpolation a different way

--- WebServer/server.js
+++ WebServer/server.js
@@ -278,8 +278,7 @@ app.post('/getuserdata', function(req, res) {
 						console.log(err);
 						res.status(400).send();
 					} else {
-						console.log(req.body.table);
-						tempCont.query("SELECT * FROM ? WHERE user_id = ?;", [req.body.table, req.user.user_id], function(err, result) {
+						tempCont.query("SELECT * FROM " + req.body.table + " WHERE user_id = " + req.user.user_id + ";", function(err, result) {
 							if(err) {
 								console.log(err);
 								res.status(400).send();
