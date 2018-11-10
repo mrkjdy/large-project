e0c8041d09b5c03f662d8c746de707c0ea7e8e21
Munchlaxx <evan.mirschel@gmail.com>
2018-11-09 20:14:17 -0500

    Attempt at fixing home page scaling.

--- StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/HomeFragment.java
+++ StepswithFriends/mobile/src/main/java/commrkjdylarge_project/github/stepwithfriends/HomeFragment.java
@@ -23,7 +23,6 @@ public class HomeFragment extends Fragment {
     int step = 0;
 
 
-
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -37,19 +36,11 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //Use this to add buttons/values/etc
         //Use getView().findViewById();
         //Refer to SettingsFragment for an example
-        final AsyncHttpClient client = ((SWFApp) getActivity().getApplication()).getClient();
-        Button testButton = getView().findViewById(R.id.button_test);
-        final TextView testText = getView().findViewById(R.id.text_test);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                testText.setText(((SWFApp) getActivity().getApplication()).getUserData("User").toString());
-            }
-        });
-    }
 
+
+    }
 }
